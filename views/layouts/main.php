0afc4e36525aa2da2c2caf276ed4f604c35a3d0f
Kristiāns Kaņeps <kanepskristians@gmail.com>
2024-12-18 20:34:23 +0200

    logo, favicon and some styling

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -5,13 +5,18 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+    <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+    <link rel="shortcut icon" href="/favicon.ico" />
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+    <link rel="manifest" href="/site.webmanifest" />
     <title><?= htmlspecialchars(isset($title) ? $title : config('APP.NAME')) ?></title>
     <link rel="stylesheet" href="/assets/css/main.css">
 </head>
 <body>
 <!-- Header -->
 <header>
-    <img src="/assets/images/logo.png" alt="<?= config('APP.NAME') ?> Logo">
+    <img src="/assets/images/logo.svg" alt="<?= config('APP.NAME') ?> Logo">
     <nav>
         <a href="<?= route('home') ?>">Home</a>
         <a href="<?= route('events') ?>">Events</a>
